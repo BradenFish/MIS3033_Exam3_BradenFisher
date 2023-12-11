eabf7da20f00192775ec431fe0489a78ea529cca
BradenFish <braden.j.fisher-1@ou.edu>
2023-12-11 13:12:17 -0600

    Add project files.

---
+++ MIS3033_Exam3_BradenFisher/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MIS3033_Exam3_BradenFisher.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MIS3033_Exam3_BradenFisher/Data/Exam3DB.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MIS3033_Exam3_BradenFisher.Data
+{
+    public class Exam3DB : DbContext// Change DbCon to your own database connect class name
+    {
+        //public DbSet<Student> Students { get; set; }// Define a table in the database. The table name here is: Students
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=129.15.67.240;Database=fish0090exam3;Port=5432;Username=fish0090;Password=jFo9OvfVJ5QFHHspd1Au");
+        }
+    }
+
+}
